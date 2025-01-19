0dac9423e330575a02004560eab273f6cfd07b94
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2025-01-19 09:03:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click',(e)=>{
+	e.preventDefault();
+	const firstName = document.getElementById('First Name').value;
+const lastName = document.getElementById('Last Name').value;
+const phoneNumber = document.getElementById('Phone Number').value;
+const emailID = document.getElementById('Email ID').value;
+	alert(`
+	   First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailID}      `)
+})
+
